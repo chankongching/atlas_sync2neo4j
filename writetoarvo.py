9717a2833778b1d0f191def63160ca9d35900fc2
Jacky Chan <chankongching@gmail.com>
2019-08-29 12:37:28 +0800

    Adding code

---
+++ writetoarvo.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import bitcoinrpc
+import yaml
+
+def getblockhash(height):
+    """taking in block height and return block hash"""
+    with open("config.yml", 'r') as ymlfile:
+        cfg = yaml.load(ymlfile)
+    url = cfg['bitcoind']['url']
+    rpc_connection = bitcoinrpc.AuthServiceProxy(url)
+    best_block_hash = rpc_connection.getbestblockhash()
+    print(rpc_connection.getblock(best_block_hash))
